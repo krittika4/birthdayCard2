11f736ea4cff59c93f1ac16cf2f3d02c5631f5ce
krittika4 <121865108+krittika4@users.noreply.github.com>
2023-06-03 15:08:29 +0530

    Add files via upload

---
+++ special.js
@@ -0,0 +1,11 @@
+function ToMainscreen(){
+    window.location = "screen.html";
+}
+
+function PictureSee(){
+    window.location = "picture.html";
+}
+
+function playGame(){
+    window.location = "index.html";
+}
